f924a769df698aed270d9c15d0b0da2d6f947387
k-aoshima <zakuzaku083@gmail.com>
2025-08-27 08:59:40 +0900

    Initial Commit

---
+++ takibi/takibiApp.swift
@@ -0,0 +1,17 @@
+//
+//  takibiApp.swift
+//  takibi
+//
+//  Created by 青嶋広輔 on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct takibiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ takibiTests/takibiTests.swift
@@ -0,0 +1,17 @@
+//
+//  takibiTests.swift
+//  takibiTests
+//
+//  Created by 青嶋広輔 on 8/27/25.
+//
+
+import Testing
+@testable import takibi
+
+struct takibiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
